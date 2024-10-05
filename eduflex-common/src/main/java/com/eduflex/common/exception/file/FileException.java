236e194f9958867d862d1f9bdca6c4f46b3965b5
Guobamao <1972212424@qq.com>
2024-10-05 14:07:25 +0800

    初始化项目

---
+++ eduflex-common/src/main/java/com/eduflex/common/exception/file/FileException.java
@@ -0,0 +1,19 @@
+package com.eduflex.common.exception.file;
+
+import com.eduflex.common.exception.base.BaseException;
+
+/**
+ * 文件信息异常类
+ * 
+ * @author ruoyi
+ */
+public class FileException extends BaseException
+{
+    private static final long serialVersionUID = 1L;
+
+    public FileException(String code, Object[] args)
+    {
+        super("file", code, args, null);
+    }
+
+}
