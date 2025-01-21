504c80b1fb3b9549ac6182d438f07db790956305
lyf <1972212424@qq.com>
2025-01-21 14:22:58 +0800

    refactor(manage): 删除学院、班级、课程年级和学期相关代码

--- eduflex-manage/src/main/java/com/eduflex/manage/domain/Teacher.java
+++ eduflex-manage/src/main/java/com/eduflex/manage/domain/Teacher.java
@@ -4,16 +4,10 @@ import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import com.eduflex.common.core.domain.entity.SysUser;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
-import org.apache.commons.lang3.builder.ToStringBuilder;
-import org.apache.commons.lang3.builder.ToStringStyle;
-import com.eduflex.common.annotation.Excel;
-import com.eduflex.common.core.domain.BaseEntity;
 
 /**
  * 教师管理对象 tb_teacher
@@ -36,8 +30,4 @@ public class Teacher extends SysUser
 
     /** 关联sys_user表的id */
     private Long userId;
-
-    /** 所属学院ID */
-    @Excel(name = "所属学院ID")
-    private Long collegeId;
 }
--- eduflex-manage/src/main/java/com/eduflex/manage/mapper/TeacherMapper.java
+++ eduflex-manage/src/main/java/com/eduflex/manage/mapper/TeacherMapper.java
@@ -2,14 +2,14 @@ package com.eduflex.manage.mapper;
 
 import java.util.List;
 
-import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.eduflex.manage.domain.Teacher;
 import org.apache.ibatis.annotations.Param;
 
 /**
  * 教师管理Mapper接口
- * 
+ *
  * @author 林煜鋒
  * @date 2024-10-05
  */
@@ -17,16 +17,16 @@ public interface TeacherMapper extends BaseMapper<Teacher>
 {
     /**
      * 查询教师管理列表
-     * 
+     *
      * @param wrapper 教师管理
      * @return 教师管理集合
      */
-    List<Teacher> selectTeacherList(@Param("ew") QueryWrapper wrapper);
+    List<Teacher> selectTeacherList(@Param("ew") LambdaQueryWrapper wrapper);
 
     /**
      * 根据id查询教师管理
      * @param id 教师ID
      * @return 教师信息
      */
-    Teacher seleteTeacherById(Long id);
+    Teacher selectTeacherById(Long id);
 }
