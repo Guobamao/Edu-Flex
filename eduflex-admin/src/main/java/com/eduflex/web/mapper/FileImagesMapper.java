55ee990afdf4e6d441db3081465010af01fee550
lyf <1972212424@qq.com>
2025-01-24 13:08:21 +0800

    feat(file): 实现文件预览功能
    - 新增 FileImages表和相关 mapper、service 用于存储和处理文件图片
    - 实现了对 PDF、Word、Excel 等文件类型的预览转换
    - 添加了文件预览的接口和页面
    - 更新 SQL 文件

---
+++ eduflex-admin/src/main/java/com/eduflex/web/mapper/FileImagesMapper.java
@@ -0,0 +1,16 @@
+package com.eduflex.web.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.eduflex.web.domain.FileImages;
+
+/**
+ * <p>
+ * 文件管理表 Mapper 接口
+ * </p>
+ *
+ * @author Lin Yufeng
+ * @since 2025-01-24
+ */
+public interface FileImagesMapper extends BaseMapper<FileImages> {
+
+}
