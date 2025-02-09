4d9a8ce7ec954fbf58eabe4f85a0d60932434643
Guobamao <1972212424@qq.com>
2025-02-09 21:28:52 +0800

    feat(manage): 添加题库管理功能

---
+++ eduflex-manage/src/main/java/com/eduflex/manage/domain/Repo.java
@@ -0,0 +1,42 @@
+package com.eduflex.manage.domain;
+
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import com.eduflex.common.annotation.Excel;
+import com.eduflex.common.core.domain.BaseEntity;
+
+/**
+ * 题库管理对象 tb_repo
+ *
+ * @author 林煜鋒
+ * @date 2025-02-09
+ */
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@TableName("tb_repo")
+public class Repo extends BaseEntity {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * 题库ID
+     */
+    private Long id;
+
+    /**
+     * 题库名称
+     */
+    @Excel(name = "题库名称")
+    private String name;
+
+    /**
+     * 关联课程
+     */
+    @Excel(name = "关联课程")
+    private Long courseId;
+}
---
+++ eduflex-manage/src/main/java/com/eduflex/manage/domain/vo/RepoVo.java
@@ -0,0 +1,20 @@
+package com.eduflex.manage.domain.vo;
+
+import com.eduflex.manage.domain.Repo;
+import lombok.Data;
+
+@Data
+public class RepoVo extends Repo {
+    private String courseName;
+
+    // 单选题数量
+    private Integer singleChoiceCount;
+    // 多选题数量
+    private Integer multipleChoiceCount;
+    // 判断题数量
+    private Integer judgeCount;
+    // 填空题数量
+    private Integer blankCount;
+    // 简答题数量
+    private Integer shortAnswerCount;
+}
