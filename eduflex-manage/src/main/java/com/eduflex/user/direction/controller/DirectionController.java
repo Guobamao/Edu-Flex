7fa76ff98c8cd533bc6f1f10e329cc0f2bfef58e
Guobamao <1972212424@qq.com>
2025-02-14 00:10:45 +0800

    feat(user): 新增用户端查询课程方向功能

---
+++ eduflex-manage/src/main/java/com/eduflex/user/direction/controller/DirectionController.java
@@ -0,0 +1,30 @@
+package com.eduflex.user.direction.controller;
+
+import com.eduflex.common.core.controller.BaseController;
+import com.eduflex.common.core.domain.AjaxResult;
+import com.eduflex.manage.direction.domain.Direction;
+import com.eduflex.manage.direction.service.IDirectionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+/**
+ * 课程方向 - 用户端Controller
+ *
+ * @author 林煜鋒
+ * @date 2025-02-13
+ */
+@RestController(value = "UserDirectionController")
+@RequestMapping("/user/direction")
+public class DirectionController extends BaseController {
+
+    @Autowired
+    private IDirectionService directionService;
+
+    @GetMapping("/list")
+    public AjaxResult list(Direction direction) {
+        return success(directionService.selectDirectionList(direction));
+    }
+}
